6ffd98cf28e30b86a67e128c6eba3662b2391685
cranci1 <100066266+cranci1@users.noreply.github.com>
2025-06-22 20:25:38 +0200

    uploaded code

---
+++ Sybau/SybauApp.swift
@@ -0,0 +1,17 @@
+//
+//  SybauApp.swift
+//  Sybau
+//
+//  Created by Francesco on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SybauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
